0d2d203f9c0731e34ab6d1f726372e160583d52f
jonsoku2 <whdtjr2792@gmail.com>
2022-07-09 19:50:26 +0900

    set typescript library in an Nx Workspace

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
-import NxWelcome from './nx-welcome';
-
+import { Header } from '@nx-example-2/store/ui-shared';
+import { formatRating } from '@nx-example-2/store/util-formatters';
 import { Route, Routes, Link } from 'react-router-dom';
 
 const StyledApp = styled.div`
@@ -9,43 +9,47 @@ const StyledApp = styled.div`
 
 export function App() {
   return (
-    <StyledApp>
-      {/* <NxWelcome title="store" /> */}
+    <>
+      <Header />
+      <p>{formatRating(2.505050505)}</p>
+      <StyledApp>
+        {/* <NxWelcome title="store" /> */}
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      <div role="navigation">
-        <img src="/assets/logo.png" alt="logo" />
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
-    </StyledApp>
+        {/* START: routes */}
+        {/* These routes and navigation have been generated for you */}
+        <div role="navigation">
+          <img src="/assets/logo.png" alt="logo" />
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page-2">Page 2</Link>
+            </li>
+          </ul>
+        </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                This is the generated root route.{' '}
+                <Link to="/page-2">Click here for page 2.</Link>
+              </div>
+            }
+          />
+          <Route
+            path="/page-2"
+            element={
+              <div>
+                <Link to="/">Click here to go back to root page.</Link>
+              </div>
+            }
+          />
+        </Routes>
+        {/* END: routes */}
+      </StyledApp>
+    </>
   );
 }
 
